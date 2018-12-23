ce1701ce933942b9c124d2d49490ee7116f95013
Jakub Biesiada <biesiadajakub@icloud.com>
2018-12-23 23:56:40 +0100

    Remove unused dependencies & update scripts

--- webpack.config.js
+++ webpack.config.js
@@ -33,7 +33,7 @@ const config = {
     rules: [
       {
         test: /\.js$/,
-        exclude: /(node_modules|bower_components)/,
+        exclude: /node_modules/,
         use: {
           loader: 'babel-loader'
         }
